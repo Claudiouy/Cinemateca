d08202c03f2cbc5260ed179ad4d5b2d2af4d3fe0
Leonardo Plachot <lplachot@gmail.com>
2011-12-04 22:31:36 -0200

    corregido

--- app/webroot/js/application.js
+++ app/webroot/js/application.js
@@ -77,24 +77,6 @@ $(function() {
              });
     });
 
-$(function() {
-		$( "#dialog:ui-dialog" ).dialog( "destroy" );
-	
-		$( "#dialog-confirm" ).dialog({
-			resizable: false,
-			height:140,
-			modal: true,
-			buttons: {
-				"Delete all items": function() {
-					$( this ).dialog( "close" );
-				},
-				Cancel: function() {
-					$( this ).dialog( "close" );
-				}
-			}
-		});
-	});
-
 });
 
 
